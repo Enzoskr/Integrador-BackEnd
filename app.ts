86c77aa480cf6d3554f959963bab897c82399216
Enzoskr <escuderoenzochechu@gmail.com>
2025-10-11 00:13:24 -0300

    redeploy html

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 import { Server } from "./models/server";
-
 import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import { dirname } from "path";
+import path from "path";
+import express from "express";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const __dirname = dirname(__filename);
 const server = new Server();
 server.listen();
 
+server.app.use(express.static(path.join(__dirname, "../public")));
+
 server.app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
