b382ebd33776175768685fc486a35dfc3e99b7e0
Enzoskr <escuderoenzochechu@gmail.com>
2025-12-29 14:37:18 -0300

    fix vercel server error 404

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import { Server } from "./models/server";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import express from "express";
+import path from "path";
 
 dotenv.config();
 
@@ -9,5 +11,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const server = new Server();
+
+server.app.use(express.static(path.join(process.cwd(), "public")));
+
+// 👇 Ruta raíz
+server.app.get("/", (_req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
 
 export default server.app;
--- dist/helpers/constants.js
+++
@@ -1,4 +0,0 @@
-export const ROLES = {
-    admin: "Administrador7",
-    user: "User7",
-};
