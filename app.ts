fc2e58cf3f82bd9e933e11450a2dff54268cd3b5
Enzoskr <escuderoenzochechu@gmail.com>
2025-12-29 14:23:48 -0300

    fix vercel html+backend

--- app.ts
+++ app.ts
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 import { Server } from "./models/server";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import path from "path";
-import express from "express";
 
 dotenv.config();
 
@@ -11,10 +9,5 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const server = new Server();
-server.listen();
 
-server.app.use(express.static(path.join(__dirname, "../public")));
-
-server.app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+export default server.app;
--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,15 @@
 import dotenv from "dotenv";
 import { Server } from "./models/server";
 import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import { dirname } from "path";
+import path from "path";
+import express from "express";
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const server = new Server();
 server.listen();
+server.app.use(express.static(path.join(__dirname, "../public")));
 server.app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
